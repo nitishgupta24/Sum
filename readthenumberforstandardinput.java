a73e4a3b05e7892698386d0005e2f747dbbcad3c
NITISH GUPTA <132565384+nitishgupta24@users.noreply.github.com>
2023-05-17 22:26:55 +0530

    Add files via upload

---
+++ readthenumberforstandardinput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class readthenumberforstandardinput {
+    public static void main(String[] args) {
+        int num;
+        System.out.println("enter the number");
+        Scanner sc=new  Scanner (System.in);  
+        num=sc.nextInt(); 
+        System.out.println("enter number is" +num);
+    }
+    
+}
