ca8901bc5bd1da5d84cf969ae6af7e59aacce767
NITISH GUPTA <132565384+nitishgupta24@users.noreply.github.com>
2023-05-17 22:50:42 +0530

    Add files via upload

---
+++ multiplicationoftwofloatnumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class multiplicationoftwofloatnumber {
+    public static void main(String[] args) {
+        System.out.println("\033c");
+        float n1;
+        float n2;
+        int mul;
+        System.out.println("enter the first number");
+        Scanner sc=new Scanner(System.in);
+        n1=sc.nextInt();
+        System.out.println("enter the second number");
+        Scanner s=new Scanner(System.in);
+        n2=s.nextInt();
+        System.out.println("\033c");
+        float a=n1*n2;
+        System.out.println("multiplication of number is" +a);
+        
+    }
+    
+}
