a45a55401a0bbfbd5b69c5bf50c51afb5b51c1d7
NITISH GUPTA <132565384+nitishgupta24@users.noreply.github.com>
2023-05-23 23:47:07 +0530

    Add files via upload

---
+++ primenumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class primenumber {
+    public static void main(String[] args) {
+        System.out.println("\033c");
+        int n;
+        System.out.println("enter the number");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("prime number");
+        }
+        else if(n%2==1)
+        {
+            System.out.println("not prime number");
+        }
+        
+            
+           
+        }
+        
+
+    }
+    
+
