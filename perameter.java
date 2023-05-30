1395a1c53721c2b22ab28e06a9162acdc5ea0337
NITISH GUPTA <132565384+nitishgupta24@users.noreply.github.com>
2023-05-30 23:58:20 +0530

    Add files via upload

---
+++ perameter.java
@@ -0,0 +1,13 @@
+public class perameter {
+    public static void main(String[] args) {
+        int length=10;
+        int breadth=20;
+         int perimeter = 2 * (length + breadth);
+       
+        System.out.println("The perimeter of the given rectangle of length "
+            + length + " and breadth " + breadth+ " = "
+            + perimeter);
+    }
+    }
+    
+
